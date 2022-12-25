a7ae2206e520f2bddafd2182bcee4e3ec82b1744
KAI <a90605190@gmail.com>
2022-12-25 17:47:10 +0900

    modified

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -4,8 +4,29 @@ const Footer = () => {
 
     return(
       <footer className='footer'>
-          <div className='contents'>
-              <h2 className='title'>여기는 푸터다.</h2>
+        <div className='footer-bottom'>
+          <div className='footer-logo'>
+            2022 LET'SPLAY.
+          </div>
+          <div className='footer-service'>
+            <ul>
+                <li>고객센터</li>
+                <li>공지사항</li>
+                <li>이용약관</li>
+                <li>개인정보처리방침</li>
+                <li>위치기반서비스 이용약관</li>
+            </ul>
+          </div>
+          </div>
+          <div className='footer-info'>
+            <div className='letpl'>랫플</div>
+            <div className='footer-last'>
+                <ul>
+                    <li>주소: 서울특별시 대치동 선릉역 2호선</li>
+                    <li>이메일: qwerss7@gmail.com</li>
+                    <li>사업자정보 : 00101101-10101000-11101</li>
+                </ul>
+            </div>
           </div>
       </footer>
     )
