3f8058172c17f91350f2a0e288844e883383acd1
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-01-10 10:58:20 +0900

    sports.js commit

---
+++ src/components/layout/board_list/sports.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { sportsButtons } from "./datacategory";
+
+const Sports = () => {
+
+  return(
+
+    <div>
+   
+  </div>
+
+  )
+
+}
+
+
+export default Sports;
